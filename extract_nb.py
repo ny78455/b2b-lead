c40257afefc6d18290bf7a1fbc7d659b46a041f3
Nitin Yadav <ny78455@gmail.com>
2026-08-10 17:59:52 +0530

    added extract nb

--- extract_nb.py
+++
@@ -1,7 +0,0 @@
-import json
-nb = json.load(open('Business_scraper.ipynb', encoding='utf-8'))
-for i, c in enumerate(nb['cells']):
-    if c['cell_type'] == 'code':
-        print(f'--- Cell {i} ---')
-        print(''.join(c['source']))
-        print()
